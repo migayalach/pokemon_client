be6f077fb7f563674282d2afba77129edeeda32a
migayalach <ayalachavezmiguel@gmail.com>
2024-01-04 21:59:42 -0400

    se despacha datos a redux

--- pokemon_client/src/views/login/Login.jsx
+++ pokemon_client/src/views/login/Login.jsx
@@ -3,20 +3,30 @@ import Form from "../../components/Form/Form";
 
 // HOOK'S
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 // LIBRARY
 
 //REDUX
+import { loginUser } from "../../redux/actions";
 
 // JAVASCRIP
 
 // STYLESHEET'S
 
-function Login() {
+function Login({ accessPage }) {
+  const dispatch = useDispatch();
+
+  const dataDispatch = async (data) => {
+    const responseLogin = dispatch(loginUser(data));
+    // console.log(responseLogin);
+    // accessPage(true);
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <Form />
+      <Form dataDispatch={dataDispatch} />
     </div>
   );
 }
