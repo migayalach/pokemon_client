bdc0b64caf7892f46f0101e771fd75d85e24ae4e
migayalach <ayalachavezmiguel@gmail.com>
2024-01-04 21:56:49 -0400

    rutas y componentes exportados

--- pokemon_client/src/App.js
+++ pokemon_client/src/App.js
@@ -8,8 +8,18 @@ import {
   About,
   Error,
 } from "./views/index";
+import NavBar from "./components/NavBar/NavBar";
 
 // HOOK'S
+import React, { useState, useEffect } from "react";
+import {
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 // LIBRARY
 
@@ -21,11 +31,34 @@ import {
 import "./styleSheets/App.css";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isLoadingPage = location.pathname === "/";
+  const [access, setAccess] = useState(false);
+
+  const accessPage = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <Login />
+      {!isLoadingPage && <NavBar />}
+      <Routes>
+        <Route path="/" element={<Login accessPage={accessPage} />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/404" element={<Error />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <Route path="/favorite" element={</>}/> */
+}
+{
+  /* <Route path="/detail" element={}/> */
+}
